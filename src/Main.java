85331bc16649e8b3339aead30e910b4f69fbbc1c
XLopicX <150371738+XLopicX@users.noreply.github.com>
2025-01-28 22:31:52 +0100

    Refactoritzat codi de main

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,34 @@
 * @author Flor Martinez
 */
 public class Main {
-	 public static void main(String[] args) {
-		Account myAccount;
-	        
-	    myAccount = new Account("Flor Martinez", "1000-1234-56-123456789", 2500);
-	     
-	    try {
-	    	myAccount.withdrawAmount(4300);
+	public static Account myAccount = new Account("Flor Martinez", "1000-1234-56-123456789", 2500);
+	
+	public static void withdrawAmount(int amount) {
+		try {
+	    	myAccount.withdrawAmount(amount);
 	    } catch(Exception e){
-        	System.err.println(e.getMessage());
-            System.out.println("Error al retirar");
+	    	System.err.println(e.getMessage());
+	        System.out.println("Error al retirar");
 	    }
-	     
-	    try {
+	}
+	
+	public static void depositAmount(int amount) {
+		try {
 	    	 System.out.println("Ingrés al compte");
-	    	 myAccount.depositAmount(1695);
+	    	 myAccount.depositAmount(amount);
 	    } catch(Exception e){
 	    	 System.err.println(e.getMessage());
 	         System.out.println("Error en l'ingrés");
 	    }
-
-	     System.out.println("El saldo actual es " + myAccount.getBalance());   
-	 }
+	}
+	
+	public static void saldo() {
+		System.out.println("El saldo actual es " + myAccount.getBalance());   
+	}
+	
+	public static void main(String[] args) {
+		withdrawAmount(2300);
+		depositAmount(1695);
+		saldo();
+	}
 }
